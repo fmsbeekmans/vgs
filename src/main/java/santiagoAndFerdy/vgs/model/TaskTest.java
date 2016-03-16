64342369f6a44f966a6cc3dea8b4e70d4a09541c
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-16 14:31:23 +0100

    stubs and experiments

---
+++ src/main/java/santiagoAndFerdy/vgs/model/TaskTest.java
@@ -0,0 +1,38 @@
+package santiagoAndFerdy.vgs.model;
+
+import com.linkedin.parseq.*;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by Fydio on 3/16/16.
+ */
+public class TaskTest {
+    public static void main(String[] args) throws InterruptedException {
+        final int numCores = Runtime.getRuntime().availableProcessors();
+        final ExecutorService taskScheduler = Executors.newFixedThreadPool(numCores + 1);
+        final ScheduledExecutorService timerScheduler = Executors.newSingleThreadScheduledExecutor();
+
+        final Engine engine = new EngineBuilder()
+                .setTaskExecutor(taskScheduler)
+                .setTimerScheduler(timerScheduler)
+                .build();
+
+        Task<Integer> toInt = Task.value(1);
+        Task<Integer> twice = toInt.map(i -> {
+            System.out.println(i);
+            System.out.println(i);
+
+            return i + i;
+        });
+
+        Thread.sleep(3000);
+
+        engine.run(twice);
+        engine.run(twice);
+    }
+
+    public static Task<Integer> slow(int i) {
+        Task.as
+    }
+}
---
+++ src/main/java/santiagoAndFerdy/vgs/model/messages/Ack.java
@@ -0,0 +1,11 @@
+package santiagoAndFerdy.vgs.model.messages;
+
+/**
+ * Created by Fydio on 3/7/16.
+ */
+public class Ack {
+
+    public static Ack instance = new Ack();
+
+    private Ack() {}
+}
