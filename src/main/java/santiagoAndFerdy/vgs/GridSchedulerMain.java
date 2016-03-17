b7407be89fcee72ca485aad9e88dd23993fb8b4b
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-18 00:55:16 +0100

    GS main stub

---
+++ src/main/java/santiagoAndFerdy/vgs/GridSchedulerMain.java
@@ -0,0 +1,10 @@
+package santiagoAndFerdy.vgs;
+
+/**
+ * Created by Fydio on 3/18/16.
+ */
+public class GridSchedulerMain {
+    public static void main(String[] args) {
+        System.out.println("I'm a grid scheduler");
+    }
+}
