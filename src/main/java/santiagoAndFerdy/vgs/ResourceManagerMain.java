a4c83c09e42eaa37426249bff103342b04e0c9cc
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-18 00:35:19 +0100

    Stub mains for user and RM

---
+++ src/main/java/santiagoAndFerdy/vgs/ResourceManagerMain.java
@@ -0,0 +1,10 @@
+package santiagoAndFerdy.vgs;
+
+/**
+ * Created by Fydio on 3/18/16.
+ */
+public class ResourceManagerMain {
+    public static void main(String[] args) {
+        System.out.println("I'm a resource manager");
+    }
+}
