20aef66ef370b4a7e7df72343edb01094ab3eb4b
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-18 01:47:17 +0100

    RMI example

--- src/main/java/santiagoAndFerdy/vgs/ResourceManagerMain.java
+++ src/main/java/santiagoAndFerdy/vgs/ResourceManagerMain.java
@@ -1,10 +1,18 @@
 package santiagoAndFerdy.vgs;
 
+import santiagoAndFerdy.vgs.rmi.MessageImplementation;
+import santiagoAndFerdy.vgs.rmi.RMIServer;
+
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+
 /**
  * Created by Fydio on 3/18/16.
  */
 public class ResourceManagerMain {
-    public static void main(String[] args) {
-        System.out.println("I'm a resource manager");
+    public static void main(String[] args) throws RemoteException, MalformedURLException {
+        RMIServer server = new RMIServer();
+        server.register("test", new MessageImplementation());
+        while (true) {}
     }
 }
---
+++ src/main/java/santiagoAndFerdy/vgs/rmi/MessageProtocol.java
@@ -0,0 +1,11 @@
+package santiagoAndFerdy.vgs.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by Fydio on 3/18/16.
+ */
+public interface MessageProtocol extends Remote {
+    String getMessage() throws RemoteException;
+}
