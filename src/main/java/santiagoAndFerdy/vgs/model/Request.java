eacd645c51c86b4ddd316f9b7692739a95b64765
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-19 00:55:34 +0100

    eq/hash

--- src/main/java/santiagoAndFerdy/vgs/model/Request.java
+++ src/main/java/santiagoAndFerdy/vgs/model/Request.java
@@ -19,4 +19,23 @@ public class Request {
     public IUser getUser() {
         return user;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Request)) return false;
+
+        Request request = (Request) o;
+
+        if (j != null ? !j.equals(request.j) : request.j != null) return false;
+        return user != null ? user.equals(request.user) : request.user == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = j != null ? j.hashCode() : 0;
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        return result;
+    }
 }
