1dfd443aa19bf4f41fed9fb75394f3ee074f66ef
--replace-all <--replace-all>
2016-03-19 00:45:45 +0100

    Removed clutter

--- src/main/java/santiagoAndFerdy/vgs/model/cluster/ICluster.java
+++
@@ -1,7 +0,0 @@
-package santiagoAndFerdy.vgs.model.cluster;
-
-/**
- * Created by Fydio on 3/4/16.
- */
-public interface ICluster {
-}
---
+++ src/main/java/santiagoAndFerdy/vgs/model/cluster/ResourceManagerProxy.java
@@ -0,0 +1,9 @@
+package santiagoAndFerdy.vgs.model.cluster;
+
+/**
+ * Created by Fydio on 3/19/16.
+ */
+public class ResourceManagerProxy {
+
+
+}
