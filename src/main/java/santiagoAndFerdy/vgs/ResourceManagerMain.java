750d06b793329de353f6fe05afefeb024797ad55
--replace-all <--replace-all>
2016-03-21 02:01:59 +0100

    WIP RM implementation

--- src/main/java/santiagoAndFerdy/vgs/ResourceManagerMain.java
+++ src/main/java/santiagoAndFerdy/vgs/ResourceManagerMain.java
@@ -1,7 +1,7 @@
 package santiagoAndFerdy.vgs;
 
-import santiagoAndFerdy.vgs.rmi.MessageImplementation;
-import santiagoAndFerdy.vgs.rmi.RMIServer;
+import santiagoAndFerdy.vgs.model.cluster.EagerResourceManager;
+import santiagoAndFerdy.vgs.rmi.RmiServer;
 
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
@@ -11,8 +11,11 @@ import java.rmi.RemoteException;
  */
 public class ResourceManagerMain {
     public static void main(String[] args) throws RemoteException, MalformedURLException {
-        RMIServer server = new RMIServer();
-        server.register("test", new MessageImplementation());
+        RmiServer server = new RmiServer(1099);
+
+        EagerResourceManager rmImpl = new EagerResourceManager(8);
+        server.register("localhost/rm", rmImpl);
+
         while (true) {}
     }
 }
--- src/main/java/santiagoAndFerdy/vgs/rmi/RmiServer.java
+++ src/main/java/santiagoAndFerdy/vgs/rmi/RmiServer.java
@@ -10,20 +10,20 @@ import java.rmi.server.UnicastRemoteObject;
 /**
  * Created by Fydio on 3/18/16.
  */
-public class RMIServer {
-    public RMIServer() {
+public class RmiServer {
+    public RmiServer(int port) {
         System.out.println("Start server");
 
         try {
-            LocateRegistry.createRegistry(1099);
+            LocateRegistry.createRegistry(port);
             System.out.println("Registery created");
         } catch (RemoteException e) {
             System.out.println("Registery already exsists");
         }
     }
 
-    public void register(String name, UnicastRemoteObject impl) throws MalformedURLException, RemoteException {
-        Naming.rebind("//localhost/" + name, impl);
-        System.out.println("Registered object at" + name);
+    public void register(String url, UnicastRemoteObject impl) throws MalformedURLException, RemoteException {
+        Naming.rebind(url, impl);
+        System.out.println("Registered object at" + url);
     }
 }
