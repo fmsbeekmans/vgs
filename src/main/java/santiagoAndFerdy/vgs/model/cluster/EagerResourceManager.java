14d1b38366ce8dd4ec86f4ebc133e3221b514aea
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-22 21:13:06 +0100

    Async RM

--- src/main/java/santiagoAndFerdy/vgs/model/cluster/EagerResourceManager.java
+++ src/main/java/santiagoAndFerdy/vgs/model/cluster/EagerResourceManager.java
@@ -1,5 +1,7 @@
 package santiagoAndFerdy.vgs.model.cluster;
 
+import com.linkedin.parseq.Engine;
+import com.linkedin.parseq.EngineBuilder;
 import com.linkedin.parseq.Task;
 import com.sun.istack.internal.NotNull;
 import org.apache.commons.collections4.queue.CircularFifoQueue;
@@ -10,7 +12,6 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.Optional;
 import java.util.Queue;
 import java.util.concurrent.*;
 
@@ -22,7 +23,8 @@ public class EagerResourceManager extends UnicastRemoteObject implements IResour
     private Queue<Node> idleNodes;
     private int n;
 
-    private ScheduledExecutorService executorService;
+    private ScheduledExecutorService timerScheduler;
+    private Engine engine;
 
     public EagerResourceManager(int n) throws RemoteException {
         super();
@@ -35,27 +37,43 @@ public class EagerResourceManager extends UnicastRemoteObject implements IResour
 
         for(int i = 0; i < n; i++) idleNodes.add(new Node(i, this));
 
-        executorService = Executors.newSingleThreadScheduledExecutor();
+        // setup async machinery
+        timerScheduler = Executors.newSingleThreadScheduledExecutor();
+        int numCores = Runtime.getRuntime().availableProcessors();
+        ExecutorService taskScheduler = Executors.newFixedThreadPool(numCores + 1);
+
+        engine = new EngineBuilder()
+                .setTaskExecutor(taskScheduler)
+                .setTimerScheduler(timerScheduler)
+                .build();
     }
 
     @Override
     public synchronized void queue(Request req) throws RemoteException, MalformedURLException, NotBoundException {
         jobQueue.add(req);
         System.out.println("Received job " + req.getJob().getJobId());
-        processQueue();
+
+        Task<Void> process = Task.action(() -> processQueue());
+        engine.run(process);
     }
 
     @Override
     public void respond(Request req) throws RemoteException, MalformedURLException, NotBoundException {
-        IResourceManagerProxy client = (IResourceManagerProxy) Naming.lookup(req.getUser().getUrl());
-        client.acceptResult(req.getJob());
+        Task<Void> respond = Task.action(() -> {
+            IResourceManagerProxy client = (IResourceManagerProxy) Naming.lookup(req.getUser().getUrl());
+            client.acceptResult(req.getJob());
+        });
+
+        engine.run(respond);
     }
 
     @Override
     public synchronized void finish(Node node, Request req) throws RemoteException, NotBoundException, MalformedURLException {
         respond(req);
         idleNodes.add(node);
-        processQueue();
+
+        Task<Void> process = Task.action(() -> processQueue());
+        engine.run(process);
     }
 
     // should be ran after any change in node state or job queue
@@ -64,13 +82,15 @@ public class EagerResourceManager extends UnicastRemoteObject implements IResour
             Node allocatedNode = idleNodes.poll();
             Request req = jobQueue.poll();
             System.out.println("Running job " + req.getJob().getJobId());
-            allocatedNode.handle(req);
+
+            Task<Void> run = Task.action(() -> allocatedNode.handle(req));
+            engine.run(run);
         }
     }
 
     @Override
     @NotNull
     public ScheduledExecutorService executorService() throws RemoteException {
-        return executorService;
+        return timerScheduler;
     }
 }
