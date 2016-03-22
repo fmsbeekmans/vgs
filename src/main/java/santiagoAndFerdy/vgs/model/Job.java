6def8bf522f7bba264e74133a807b7f0915171d9
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-22 20:31:26 +0100

    Job equals/hash based on id + rm id

--- src/main/java/santiagoAndFerdy/vgs/model/Job.java
+++ src/main/java/santiagoAndFerdy/vgs/model/Job.java
@@ -1,51 +1,70 @@
 package santiagoAndFerdy.vgs.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * Created by Fydio on 3/3/16.
  */
 public class Job implements Serializable {
-	private int		duration;
-	private long	jobId;
-	private long	initialResourceManagerId;
-	private int[]	additionalResourceManagerIds;
+    private int duration;
+    private long jobId;
+    private long initialResourceManagerId;
+    private int[] additionalResourceManagerIds;
 
-	public Job(int duration, long jobId, long initialResourceManagerId) {
-		this.duration = duration;
-		this.jobId = jobId;
-		this.initialResourceManagerId = initialResourceManagerId;
-	}
+    public Job(int duration, long jobId, long initialResourceManagerId) {
+        this.duration = duration;
+        this.jobId = jobId;
+        this.initialResourceManagerId = initialResourceManagerId;
+    }
 
-	public int getDuration() {
-		return duration;
-	}
+    public int getDuration() {
+        return duration;
+    }
 
-	public long getJobId() {
-		return jobId;
-	}
+    public long getJobId() {
+        return jobId;
+    }
 
-	public long getInitialResourceManagerId() {
-		return initialResourceManagerId;
-	}
+    public long getInitialResourceManagerId() {
+        return initialResourceManagerId;
+    }
 
-	public int[] getAdditionalResourceManagerIds() {
-		return (additionalResourceManagerIds != null) ? additionalResourceManagerIds : new int[0];
-	}
+    public int[] getAdditionalResourceManagerIds() {
+        return (additionalResourceManagerIds != null) ? additionalResourceManagerIds : new int[0];
+    }
 
-	public void addResourceManagerIds(int newResourceManagerId) {
-		if (additionalResourceManagerIds == null) {
-			additionalResourceManagerIds = new int[] { newResourceManagerId };
-		} else {
-			int n = additionalResourceManagerIds.length;
-			int[] swap = new int[n + 1];
+    public void addResourceManagerIds(int newResourceManagerId) {
+        if (additionalResourceManagerIds == null) {
+            additionalResourceManagerIds = new int[] { newResourceManagerId };
+        }
+        else {
+            int n = additionalResourceManagerIds.length;
+            int[] swap = new int[n + 1];
 
-			for (int i = 0; i < n; i++)
-				swap[i] = additionalResourceManagerIds[i];
+            for(int i = 0; i < n; i++) swap[i] = additionalResourceManagerIds[i];
 
-			swap[n] = newResourceManagerId;
+            swap[n] = newResourceManagerId;
 
-			additionalResourceManagerIds = swap;
-		}
-	}
+            additionalResourceManagerIds = swap;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+
+        Job job = (Job) o;
+
+        if (jobId != job.jobId) return false;
+        return initialResourceManagerId == job.initialResourceManagerId;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (jobId ^ (jobId >>> 32));
+        result = 31 * result + (int) (initialResourceManagerId ^ (initialResourceManagerId >>> 32));
+        return result;
+    }
 }
