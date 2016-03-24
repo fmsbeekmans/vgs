efe0c4af3a81b0d307cd3975124f19d3e7f23841
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-24 01:00:01 +0100

    Repository is generic

--- src/main/java/santiagoAndFerdy/vgs/discovery/Repository.java
+++ src/main/java/santiagoAndFerdy/vgs/discovery/Repository.java
@@ -3,50 +3,41 @@ package santiagoAndFerdy.vgs.discovery;
 import santiagoAndFerdy.vgs.model.cluster.IResourceManagerDriver;
 import santiagoAndFerdy.vgs.model.cluster.IResourceManagerUserClient;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * Created by Fydio on 3/24/16.
  */
-public class Repository implements IRepository {
+public class Repository<T extends Serializable> implements IRepository<T> {
 
-    private String[] userUrls;
-    private Status[] userStatuses;
-    private String[] resourceManagerUrls;
-    private Status[] resourceManagerStatuses;
+    private String[] urls;
+    private Status[] statuses;
 
-    public Repository(Map<Integer, String> userUrls, Map<Integer, String> resourceManagerUrls) {
-        int nUsers = userUrls.keySet().stream().max(Comparator.naturalOrder()).orElse(0);
-        this.userUrls = new String[nUsers];
-        this.userStatuses = new Status[nUsers];
+    public Repository(Map<Integer, String> urls) {
+        int n = urls.keySet().stream().max(Comparator.naturalOrder()).orElse(0);
+        this.urls = new String[n];
+        this.statuses = new Status[n];
 
-        for(int k : userUrls.keySet()) {
-            this.userUrls[k] = userUrls.get(k);
-            this.userStatuses[k] = Status.OFFLINE;
-        }
-
-        int nResourceManagers = userUrls.keySet().stream().max(Comparator.naturalOrder()).orElse(0);
-        this.resourceManagerUrls = new String[nResourceManagers];
-        this.resourceManagerStatuses = new Status[nResourceManagers];
-
-        for(int k : resourceManagerUrls.keySet()) {
-            this.resourceManagerUrls[k] = resourceManagerUrls.get(k);
-            this.resourceManagerStatuses[k] = Status.OFFLINE;
+        for(int k : urls.keySet()) {
+            this.urls[k] = urls.get(k);
+            this.statuses[k] = Status.OFFLINE;
         }
     }
 
     @Override
-    public Optional<IResourceManagerDriver> getResourceManager(int id) {
-        return Optional.ofNullable(resourceManagerUrls[id])
+    public Optional<T> getEntity(int id) {
+        return Optional.ofNullable(urls[id])
                 .flatMap(url -> {
                     try {
-                        return Optional.of((IResourceManagerDriver) Naming.lookup(url));
+                        return Optional.of((T) Naming.lookup(url));
                     } catch (NotBoundException | MalformedURLException | RemoteException e) {
                         e.printStackTrace();
                         return Optional.empty();
@@ -55,42 +46,14 @@ public class Repository implements IRepository {
     }
 
     @Override
-    public Optional<Status> getLastKnownResourceManagerStatus(int id) {
-        return Optional.ofNullable(resourceManagerStatuses[id]);
+    public Optional<Status> getLastKnownStatus(int id) {
+        return Optional.ofNullable(statuses[id]);
     }
 
     @Override
-    public boolean setLastKnownResourceManagerStatus(int id, Status newStatus) {
-        if(resourceManagerStatuses[id] != null) {
-            resourceManagerStatuses[id] = newStatus;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public Optional<IResourceManagerUserClient> getUserClient(int id) {
-        return Optional.ofNullable(userUrls[id])
-                .flatMap(url -> {
-                    try {
-                        return Optional.of((IResourceManagerUserClient) Naming.lookup(url));
-                    } catch (NotBoundException | MalformedURLException | RemoteException e) {
-                        e.printStackTrace();
-                        return Optional.empty();
-                    }
-                });
-    }
-
-    @Override
-    public Optional<Status> getLastKnownUserClientStatus(int id) {
-        return Optional.ofNullable(userStatuses[id]);
-    }
-
-    @Override
-    public boolean setLastKnownUserStatus(int id, Status newStatus) {
-        if(userStatuses[id] != null) {
-            userStatuses[id] = newStatus;
+    public boolean setLastKnownStatus(int id, Status newStatus) {
+        if(statuses[id] != null) {
+            statuses[id] = newStatus;
             return true;
         } else {
             return false;
