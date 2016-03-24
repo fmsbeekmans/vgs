a1222fd5c3e035b6d3d3506348d2da02bbad242b
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-24 01:00:01 +0100

    Repository is generic

--- src/main/java/santiagoAndFerdy/vgs/discovery/IRepository.java
+++ src/main/java/santiagoAndFerdy/vgs/discovery/IRepository.java
@@ -9,11 +9,11 @@ import java.util.Optional;
 /**
  * Created by Fydio on 3/24/16.
  */
-public interface IRepository extends Serializable {
+public interface IRepository<T extends Serializable> extends Serializable {
 
     // for user
-    Optional<IResourceManagerDriver> getResourceManager(int id);
-    Optional<Status> getLastKnownResourceManagerStatus(int id);
+    Optional<T> getEntity(int id);
+    Optional<Status> getLastKnownStatus(int id);
 
     /**
      * Updates the last known status of an RM
@@ -21,16 +21,5 @@ public interface IRepository extends Serializable {
      * @param newStatus the state that the RM is currently thought to be in
      * @return true if the rm is known, otherwise false
      */
-    boolean setLastKnownResourceManagerStatus(int id, Status newStatus);
-
-    // for resource managers
-    Optional<IResourceManagerUserClient> getUserClient(int id);
-    Optional<Status> getLastKnownUserClientStatus(int id);
-    /**
-     * Updates the last known status of an RM
-     * @param id the rm id
-     * @param newStatus the state that the RM is currently thought to be in
-     * @return true if the rm is known, otherwise false
-     */
-    boolean setLastKnownUserStatus(int id, Status newStatus);
+    boolean setLastKnownStatus(int id, Status newStatus);
 }
