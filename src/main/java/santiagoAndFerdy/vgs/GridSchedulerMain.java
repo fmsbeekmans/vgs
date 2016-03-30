bfe27d3c199cbdb74c6023ebe2518570012b9abf
Ferdy Beekmans <ferdy.beekmans@zivver.com>
2016-03-30 12:04:57 +0200

    rm/gs mains

--- src/main/java/santiagoAndFerdy/vgs/GridSchedulerMain.java
+++ src/main/java/santiagoAndFerdy/vgs/GridSchedulerMain.java
@@ -1,24 +1,42 @@
 package santiagoAndFerdy.vgs;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
+import java.util.Map;
 
-import santiagoAndFerdy.vgs.gridScheduler.GridScheduler;
+import santiagoAndFerdy.vgs.discovery.IRepository;
+import santiagoAndFerdy.vgs.discovery.Repository;
+import santiagoAndFerdy.vgs.gridScheduler.GridSchedulerDriver;
+import santiagoAndFerdy.vgs.gridScheduler.IGridSchedulerGridSchedulerClient;
+import santiagoAndFerdy.vgs.resourceManager.IResourceManagerGridSchedulerClient;
 import santiagoAndFerdy.vgs.rmi.RmiServer;
 
 /**
  * Created by Fydio on 3/18/16.
  */
 public class GridSchedulerMain {
-    public static void main(String[] args) throws RemoteException, MalformedURLException, InterruptedException, NotBoundException {
-        System.out.println("I'm a grid scheduler");
-        RmiServer server = new RmiServer(1099);
-        GridScheduler gs = new GridScheduler(server);
-        server.register("localhost/gs", gs);
-        gs.startHBHandler();
-        while (true) {
+    public static void main(String[] args) throws IOException, InterruptedException, NotBoundException, URISyntaxException {
+        RmiServer rmiServer = new RmiServer(1099);
 
+        URL url = UserMain.class.getClassLoader().getResource("gs/rms");
+        Path rmRepositoryFilePath = Paths.get(url.toURI());
+        IRepository<IResourceManagerGridSchedulerClient> rmRepository = Repository.fromFile(rmRepositoryFilePath);
+        url = UserMain.class.getClassLoader().getResource("gs/gss");
+        Path gsRepositoryFilePath = Paths.get(url.toURI());
+        IRepository<IGridSchedulerGridSchedulerClient> gsRepository = Repository.fromFile(gsRepositoryFilePath);
+        Map<Integer, String> gridSchedulerUrls = gsRepository.urls();
+
+        for(int gsId : gsRepository.ids()) {
+            GridSchedulerDriver driver = new GridSchedulerDriver(
+                    rmiServer,
+                    rmRepository,
+                    gsRepository,
+                    gridSchedulerUrls.get(gsId),
+                    gsId);
         }
     }
 }
--- src/main/java/santiagoAndFerdy/vgs/ResourceManagerMain.java
+++ src/main/java/santiagoAndFerdy/vgs/ResourceManagerMain.java
@@ -2,6 +2,7 @@ package santiagoAndFerdy.vgs;
 
 import santiagoAndFerdy.vgs.discovery.IRepository;
 import santiagoAndFerdy.vgs.discovery.Repository;
+import santiagoAndFerdy.vgs.gridScheduler.IGridSchedulerResourceManagerClient;
 import santiagoAndFerdy.vgs.resourceManager.EagerResourceManager;
 import santiagoAndFerdy.vgs.resourceManager.IResourceManagerDriver;
 import santiagoAndFerdy.vgs.rmi.RmiServer;
@@ -11,26 +12,26 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.rmi.NotBoundException;
 import java.util.Map;
 
 /**
  * Created by Fydio on 3/18/16.
  */
 public class ResourceManagerMain {
-    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException, NotBoundException {
         RmiServer server = new RmiServer(1099);
         URL url = UserMain.class.getClassLoader().getResource("rm/rms");
         Path rmRepositoryFilePath = Paths.get(url.toURI());
-        IRepository<IResourceManagerDriver> repo = Repository.fromFile(rmRepositoryFilePath);
+        IRepository<IResourceManagerDriver> rmRepo = Repository.fromFile(rmRepositoryFilePath);
+        url = UserMain.class.getClassLoader().getResource("rm/gss");
+        Path gsRepositoryFilePath = Paths.get(url.toURI());
+        IRepository<IGridSchedulerResourceManagerClient> gsRepo = Repository.fromFile(gsRepositoryFilePath);
 
-        Map<Integer, String> rmUrls = repo.urls();
-        //for(int id : rmUrls.keySet()) {
-        // Thread.sleep(2000);
-        EagerResourceManager rmImpl = new EagerResourceManager(0, 10000, server);
-        server.register(rmUrls.get(0), rmImpl);
-        rmImpl.startHBHandler();
-        //}
-
-        while (true) {}
+        Map<Integer, String> rmUrls = rmRepo.urls();
+        for(int id : rmUrls.keySet()) {
+            EagerResourceManager rmImpl = new EagerResourceManager(id, 10000, rmUrls.get(id), server, gsRepo);
+            server.register(rmUrls.get(0), rmImpl);
+        }
     }
 }
