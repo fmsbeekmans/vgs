0aa4f0c4159fb76050c332bb90229667a3c4a4e8
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-03-30 02:09:37 +0200

    Grid scheduler driver methods

--- src/main/java/santiagoAndFerdy/vgs/gridScheduler/IGridSchedulerDriver.java
+++ src/main/java/santiagoAndFerdy/vgs/gridScheduler/IGridSchedulerDriver.java
@@ -2,6 +2,7 @@ package santiagoAndFerdy.vgs.gridScheduler;
 
 import com.sun.istack.internal.NotNull;
 import santiagoAndFerdy.vgs.messages.Heartbeat;
+import santiagoAndFerdy.vgs.model.Request;
 
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
@@ -14,5 +15,35 @@ import java.rmi.RemoteException;
 public interface IGridSchedulerDriver extends Remote {
     void ping(@NotNull Heartbeat h) throws RemoteException, MalformedURLException, NotBoundException, InterruptedException;
 
+    /**
+     * Request this grid scheduler to monitor the life-cycle for this job
+     * @param request the request to watch
+     * @throws RemoteException
+     */
+    void monitorPrimary(Request request) throws RemoteException;
 
+    /**
+     * Request this grid scheduler to also watch a job in case the primary grid scheduler crashes
+     * @param request the request to watch
+     * @throws RemoteException
+     */
+    void manitorBackUp(Request request) throws RemoteException;
+
+    /**
+     * Become the primary grid scheduler for this job in case tho previous primary grid scheduler or resource manager crash
+     * @param request the request to reschedule and rebackup
+     */
+    void promoteToPrimary(Request request);
+
+    /**
+     * For a resource manager to request a job to be scheduled elsewhere
+     * @param request to schedule somewhere else
+     */
+    void offload(Request request);
+
+    /**
+     * A job has finished. All the reserved resources can be released.
+     * @param requestId
+     */
+    void releaseResources(int requestId);
 }
